3badf9c049857292121702c54be1f1b158c9b936
fayez-baig <coderninja16@gmail.com>
2022-05-22 00:51:55 +0530

    🩹 updated stale time

--- src/app/hooks/queries-hooks/useGetResults.tsx
+++ src/app/hooks/queries-hooks/useGetResults.tsx
@@ -3,7 +3,8 @@ import { getResults } from 'app/queries';
 import { SearchResults } from 'app/domains/kernel/types';
 
 export const useGetResults = (searchTerm: string): UseQueryResult<SearchResults, Error> =>
-    useQuery([`players-info`, searchTerm], () => getResults(searchTerm), {
+    useQuery([`results-${searchTerm}`, searchTerm], () => getResults(searchTerm), {
         refetchOnWindowFocus: false,
+        staleTime: 150000,
         enabled: !!searchTerm,
     });
