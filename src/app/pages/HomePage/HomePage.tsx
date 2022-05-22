004ae78c95de4ab01a7d593b8699f772c528124c
fayez-baig <coderninja16@gmail.com>
2022-05-22 13:23:54 +0530

    🩹 fix: flikering fixed

--- src/app/pages/HomePage/HomePage.tsx
+++ src/app/pages/HomePage/HomePage.tsx
@@ -8,44 +8,46 @@ const HomePage: FC = () => {
     const { data, isFetching, isError } = useGetResults(debouncedSearchTerm);
 
     return (
-        <div className="bg-[url('/src/assets/images/bg-image.jpeg')] h-screen flex flex-col items-center justify-center w-auto p-2">
-            <div className="mb-10 text-center">
-                <h1 className="text-white drop-shadow-[2px 2px 5px rgb(0 0 0 / 15%)] text-5xl lg:text-6xl">
-                    Home away from Home
-                </h1>
-                <h2 className="text-xl text-white lg:text-2xl">
-                    Book your student accommodation near top universities across the globe
-                </h2>
+        <div className="bg-[url('/src/assets/images/bg-image.jpeg')] relative h-screen flex flex-col items-center">
+            <div className="absolute p-2 top-36">
+                <div className="mb-10 text-center">
+                    <h1 className="mb-1 text-white drop-shadow-[2px 2px 5px rgb(0 0 0 / 15%)] text-5xl lg:text-6xl">
+                        Home away from Home
+                    </h1>
+                    <h2 className="text-xl text-white lg:text-2xl">
+                        Book your student accommodation near top universities across the globe
+                    </h2>
+                </div>
+                <ReactAutoComplete
+                    isFetching={isFetching}
+                    isError={isError}
+                    // eslint-disable-next-line no-console
+                    handleSubmit={(result) => console.log(result)}
+                    defaultResult={[
+                        {
+                            title: 'Recent Searches',
+                            recentSearch: ['london', 'paris', 'berlin'],
+                        },
+                        {
+                            title: 'Popular cities',
+                            recentSearch: [
+                                'london',
+                                'paris',
+                                'berlin',
+                                'Germany',
+                                'New York',
+                                'France',
+                                'UAE',
+                                'USA',
+                                'Netherlands',
+                            ],
+                        },
+                    ]}
+                    setInputValue={setSearchTerm}
+                    inputValue={searchTerm}
+                    results={data?.data?.data?.result?.slice(0, 5) || []}
+                />
             </div>
-            <ReactAutoComplete
-                isFetching={isFetching}
-                isError={isError}
-                // eslint-disable-next-line no-console
-                handleSubmit={(result) => console.log(result)}
-                defaultResult={[
-                    {
-                        title: 'Recent Searches',
-                        recentSearch: ['london', 'paris', 'berlin'],
-                    },
-                    {
-                        title: 'Popular cities',
-                        recentSearch: [
-                            'london',
-                            'paris',
-                            'berlin',
-                            'Germany',
-                            'New York',
-                            'France',
-                            'UAE',
-                            'USA',
-                            'Netherlands',
-                        ],
-                    },
-                ]}
-                setInputValue={setSearchTerm}
-                inputValue={searchTerm}
-                results={data?.data?.data?.result?.slice(0, 5) || []}
-            />
         </div>
     );
 };
